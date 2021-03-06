853dfa5c073aa7aa2f1201f04cb3ac9a3599c68d
ShiftTGC <ShiftTGC@gmail.com>
2021-03-06 15:41:42 +0100

    Potentially complete calculator with loop and breaks

--- src/Calculator/ShiftTGC/Main.java
+++ src/Calculator/ShiftTGC/Main.java
@@ -10,8 +10,80 @@ public class Main {
         Scanner input = new Scanner(System.in); //Makes a new Scanner object. Allows user to write.
 
         println(" ------------------------------");
-        println("| Shift's Calculator - v2.0.3 |");
+        println("| Shift's Calculator - v2.2.3 |");
         println(" ------------------------------");
+        println("");
+
+        loop:
+        while (true) {
+            try //meant to help catch any
+            {
+
+                print("Input value x: ");
+                int x = input.nextInt(); //Stores user input into variable "x"
+//            println(""); //Skips to next line so things won't be connected.
+
+                print("Input symbol: ");
+                String symbol = input.next(); //Stores user input into variable "symbol"
+//            println("");
+
+                print("Input value y: ");
+                int y = input.nextInt(); //Stores user input into variable "y"
+//            println("");
+
+                println(x + symbol + y + "=" + math(symbol, x, y));
+                println("");
+
+                print("Want to exit? (Y/N): ");
+                String exit = input.next();
+
+                /*
+                exit - variable
+                .equalsIgnoreCase - Function - Tells it to ignore if it is a big y or a small Y
+                 */
+                while (true) {
+                    if (exit.equalsIgnoreCase("y"))
+                    {
+                        println("");
+                        println("Have a nice day/night!");
+                        break loop;
+                    }
+                    else if (exit.equalsIgnoreCase("n")) {
+                        println("Resetting calculator");
+                        println("");
+                    } else throw new Exception("User Didn't answer Y or N");
+
+                }
+
+
+            } catch (Exception e) {
+                input = new Scanner(System.in); //Prevents a very specific loop
+                println("ERROR");
+                println("x and y can only be integers (between -2.147.483.648 to 2.147.483.648)");
+                println("Symbols can only be:");
+                println("+ - Addition");
+                println("\"-\"");
+            }
+        }
+    }
+
+
+    private static int math(String symbol, int x, int y) throws Exception {
+        switch (symbol) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+            case "x":
+            case "X":
+                return x * y;
+            case "/":
+                return x / y;
+            case "%":
+                return x % y;
+        }
+        throw new Exception("Illegal Symbol");
     }
 
     public static void print(String text) //Makes a function named print
